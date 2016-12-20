df2182fc397d1bf0dcfee76ff19955ffeb7adac6
John Christian M. Butalid <zristheory@gmail.com>
2016-12-20 09:17:05 +0800

    Create Butalid_december_20.c

---
+++ Butalid_december_20.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+int main(){
+	
+	int month, day;
+	printf("Enter month number (1-12): ");
+    scanf("%d", &month);
+     printf("Enter day number (1-7):");
+    scanf("%d", & day);
+ 
+   
+ 
+    if(month == 1)
+    {
+        printf("january");
+    }
+  if(month == 2)
+    {
+        printf("february");
+    }
+ if(month == 3)
+    {
+        printf("march");
+    }
+    if(month == 4)
+    {
+        printf("april");
+    }
+     if(month == 5)
+    {
+        printf("may");
+    }
+     if(month == 6)
+    {
+        printf("june");
+    }
+    if(month == 7)
+    {
+        printf("july");
+    }
+  if(month == 8)
+    {
+        printf("august");
+    }
+   if(month == 9)
+    {
+        printf("september");
+    }
+    if(month == 10)
+    {
+        printf("october");
+    }
+    if(month == 11)
+    {
+        printf("november");
+    }
+     if(month == 12)
+    {
+        printf("december");
+    }
+     printf(" ");
+     if(day == 1)
+    {
+        printf("sunday");
+    }
+    if(day == 2)
+    {
+        printf("saturday");
+    }
+    if(day == 3)
+    {
+        printf("friday");
+    }
+    if(day == 4)
+    {
+        printf("thursday");
+    }
+    if(day == 5)
+    {
+        printf("wednesday");
+    }
+   if(day == 6)
+    {
+        printf("tuesday");
+    }
+    if(day == 7)
+    {
+        printf("monday");
+    }
+    
+   printf(" .\n");
+    }
+ 
