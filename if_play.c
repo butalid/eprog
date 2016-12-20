7084c2a6766b9ab0324719dd56abbcd39e9a89c1
John Christian M. Butalid <zristheory@gmail.com>
2016-12-20 08:11:39 +0800

    Create if_play.c

---
+++ if_play.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int grade;
+	
+	printf("Enter grade:");
+	scanf("%d", &grade);
+	printf("Grade is: %d", grade);
+	if(grade% 3==0){
+		printf("Ed");
+	}
+	if(grade%5==0){
+		printf("pogi");
+	}
+}
