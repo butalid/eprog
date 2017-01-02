ce68964282ef357260cd32c4b84cb619b632d5c2
John Christian M. Butalid <zristheory@gmail.com>
2017-01-03 07:42:58 +0800

    Create odd.cpp

---
+++ odd.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int counter;
+	printf("odd numbers between 1 to 100\n");
+	for(counter=1;counter<=100; counter++){
+		if(counter%2==1){
+			printf("%d",counter);
+	
+		}
+	
+	}
+	return 0;
+}
