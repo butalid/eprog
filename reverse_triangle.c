d176f85698290dd4c1e613aacd1085201813dd6a
John Christian M. Butalid <zristheory@gmail.com>
2017-01-24 07:54:10 +0800

    Create reverse_triangle.c

---
+++ reverse_triangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int l, k, rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+
+    for(l=rows; l>=1; --l)
+    {
+        for(k=1; k<=l; ++k)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    system("pause");
+}
