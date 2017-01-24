3dbe10a806ce91f2c8e081b2b818ebfe945bab76
John Christian M. Butalid <zristheory@gmail.com>
2017-01-24 09:22:42 +0800

    Create huling_sayaw.c

---
+++ huling_sayaw.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("intput: ");
+    scanf("%d", &num);
+    printf("output:%d\n", num%10);
+    printf("\nintput: ");
+    scanf("%d", &num);
+    printf("output:%d\n", num%10);
+    printf("\nintput: ");
+    scanf("%d", &num);
+    printf("output:%d\n", num%10);
+	printf("\nintput: ");
+    scanf("%d", &num);
+    printf("output:%d\n", num%10);
+}
