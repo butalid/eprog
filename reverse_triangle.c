2d2bb1fa481ffc17a7d4f89d68be15625d74b7bc
John Christian M. Butalid <zristheory@gmail.com>
2017-01-24 08:01:17 +0800

    Update reverse_triangle.c

--- reverse_triangle.c
+++ reverse_triangle.c
@@ -5,12 +5,9 @@ int main()
 
     printf("Enter number of rows: ");
     scanf("%d",&rows);
-
-    for(l=rows; l>=1; --l)
-    {
-        for(k=1; k<=l; ++k)
-        {
-            printf("* ");
+	for(l=rows; l>=1; --l){
+	for(k=1; k<=l; ++k){
+		printf("* ");
         }
         printf("\n");
     }
