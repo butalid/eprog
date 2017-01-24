db830cb4e810b6162fb679800b7282b7081a0314
John Christian M. Butalid <zristheory@gmail.com>
2017-01-24 08:45:23 +0800

    Create smallest_divisible.c

---
+++ smallest_divisible.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int y;
+	int num=0;
+	while(1){
+			y++;
+			if( y%6==0 && y%8==0 && y%9==0 && y%11==0 && y%13==0 && y%14==0 && y%15==0 && y%17==0 && y%18==0 && y%19==0 && y%20==0){
+				printf("%d",y);
+				break;
+					}
+			}
+			
+}
+
