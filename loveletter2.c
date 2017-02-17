0e74bc7ba3695bbdecd67349de0b754a321b3e6c
John Christian M. Butalid <zristheory@gmail.com>
2017-02-17 18:17:05 +0800

    Update loveletter2.c

--- loveletter2.c
+++ loveletter2.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 main(){
-char ch;
-FILE *fp;
-fp=fopen("input.txt","r");
-while(!feof(fp)){
-ch=getc(fp);
-printf("%d ",ch);
-}
+  char ch;
+  FIILE *fp;
+  fp=fopen("input.txt","r");
+  while(!feof(fp)){
+    ch=getc(fp);
+    printf("%d",ch);
+  }
 }
